428eca7979f0d67c87702255a56f2e9fe9ce00b4
vinaykumar1989 <vinay.java1989@gmail.com>
2024-12-06 09:32:20 +0200

    initail commit

---
+++ src/main/java/com/pvr/registration/AccountRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.pvr.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pvr/registration/AccountRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvr.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
