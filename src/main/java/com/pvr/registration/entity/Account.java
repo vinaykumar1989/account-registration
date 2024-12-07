fe7197685dcc267fdbe79a5ace2d046e72266ef7
vinaykumar1989 <vinay.java1989@gmail.com>
2024-12-07 10:20:11 +0200

    wertreg

--- src/main/java/com/pvr/registration/entity/Account.java
+++ src/main/java/com/pvr/registration/entity/Account.java
@@ -1,9 +1,15 @@
 package com.pvr.registration.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Account {
 
 
